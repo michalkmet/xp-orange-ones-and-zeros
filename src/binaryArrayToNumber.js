aa3f1d633e40b6fc7612a1d1e81d0bb1c8a5e982
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-10-10 10:35:32 +0200

    UAT 1.8 done

--- src/binaryArrayToNumber.js
+++ src/binaryArrayToNumber.js
@@ -3,25 +3,15 @@ function binaryArrayToNumber(arrWithDigits) {
 
   let result = 0;
 
-  for (let i = 0; i < arrWithDigits.length; i++) {
-    if (arrWithDigits[arrWithDigits.length - 1] === 1) {
-      console.log('Math.pow(2, i): ', Math.pow(2, i));
-      result += Math.pow(2, i);
+  for (let i = 1; i <= arrWithDigits.length; i++) {
+    if (arrWithDigits[arrWithDigits.length - i] === 1) {
+      if (i === 1) {
+        result += 1;
+      } else {
+        result += Math.pow(2, i - 1);
+      }
     }
   }
-
-  // if (arrWithDigits[arrWithDigits.length - 1] === 1) {
-  //   result += 1;
-  // }
-  // if (arrWithDigits[arrWithDigits.length - 2] === 1) {
-  //   result += 2;
-  // }
-  // if (arrWithDigits[arrWithDigits.length - 3] === 1) {
-  //   result += 4;
-  // }
-  // if (arrWithDigits[arrWithDigits.length - 4] === 1) {
-  //   result += 8;
-  // }
 
   console.log('result: ', result);
   return result;
